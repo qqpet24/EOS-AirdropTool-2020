bd2f04224022f90fe993379f3d8ebf3da1281384
qqpet24 <54110716+qqpet24@users.noreply.github.com>
2020-10-18 09:43:46 +0800

    Add files via upload

---
+++ airdrop.js
@@ -0,0 +1,76 @@
+﻿const { Api, JsonRpc } = require('eosjs');
+const { JsSignatureProvider } = require('eosjs/dist/eosjs-jssig');  
+const fetch = require('node-fetch'); 
+const { TextDecoder, TextEncoder } = require('util');
+const rpc = new JsonRpc('http://openapi.eos.ren', { fetch }); //API node
+const signatureProvider = new JsSignatureProvider(["//Your privte key"]);//Change this field to your account's private key
+const api = new Api({ rpc, signatureProvider , textDecoder: new TextDecoder(), textEncoder: new TextEncoder() });
+
+const account="Your account"//Change this field to your account
+var flag=1;//Airdrop to accounts after this line in filePath
+const batchAction=100;// Airdrop to 100 account in one time
+const filePath='account.txt'//The airdrop account list
+const tokenContract='eosio.token'//If EOS,it's eosio.token
+const quantity='0.0001 EOS'// If 0.0001 EOS It means that you will airdrop 0.0001 EOS to other account
+const awaitTime=1200;//ms
+
+
+
+function sleep (time) {  
+	return new Promise((resolve) => setTimeout(resolve, time));
+}
+var airdropAccounts=[];
+var fs=require('fs');
+fs.readFile(filePath,'utf-8',function(err,data){
+	if(err){
+		console.error(err);
+	}
+	else{
+		airdropAccounts=data.split("\r\n");
+	}
+});
+
+(async () =>{
+	await sleep(10000);//Waitting for reading file,等待文件读取进去
+	while(true){
+		if(flag>airdropAccounts.length){
+			break;
+		}
+		if(true){
+			try {
+				var myactions = [];
+
+				for(var i=flag;i<flag+batchAction;i++){
+					var myaction =  {
+						account: tokenContract,
+						name: 'transfer',
+						authorization: [{
+							actor:account,
+							permission: 'active',
+						}],
+						data: {
+								"from":account,
+								"to":airdropAccounts[i],
+								"quantity":quantity,
+								"memo":flag
+							  },
+					};	
+					myactions.push(myaction);
+				}
+				console.log(myactions);
+				var result=await api.transact({
+					actions: myactions
+				}, {
+					blocksBehind: 30,
+					expireSeconds: 30,
+				});
+				console.log(result)
+			} catch (e) {
+				console.log(e);
+				//console.log(e.json.error.details);
+			}
+		}
+		flag+=batchAction;
+		await sleep(awaitTime);
+	}
+})();
---
+++ airdrop_snapshots.js
@@ -0,0 +1,76 @@
+﻿const { Api, JsonRpc } = require('eosjs');
+const { JsSignatureProvider } = require('eosjs/dist/eosjs-jssig');  
+const fetch = require('node-fetch'); 
+const { TextDecoder, TextEncoder } = require('util');
+const rpc = new JsonRpc('http://openapi.eos.ren', { fetch }); 
+
+const awaitTime=10;
+const limit=5000;
+const tokenContract="eosio.token"
+const tokenTable="accounts"
+
+
+
+function sleep (time) {
+	return new Promise((resolve) => setTimeout(resolve, time));
+}
+async function getScope(code,table,lower_bound,limit){
+	let data = await rpc.get_table_by_scope({
+		code:code,
+		table:table,
+		lower_bound: lower_bound,
+		limit:limit//表行数限制
+	})
+	return data;
+}
+async function getFullTableScope(code,table){
+	var data=[];
+	var nextkey=0;
+	var isFirst=true;
+	while(true){
+		try{
+			var tmpdata=await getScope(code,table,nextkey,limit);
+			for(var i=0;i<Object.keys(tmpdata.rows).length;i++){
+				if(isFirst==true){
+					isFirst=false;
+				}else if(i==0){
+					continue;
+				}
+				data.push(tmpdata.rows[i]);
+			}
+			nextkey=tmpdata.more;
+			console.log(tmpdata);
+			if(tmpdata.more==''){
+				break;
+			}
+		}catch(e){
+			console.log(e)
+		}
+		await sleep(awaitTime);
+	}
+	return data;
+}
+async function writeFile(filePath,message){//写入文件，自动换行
+	var fs = require('fs'); 
+	fs.writeFile(filePath, message+'\r\n', { 'flag': 'a' }, function(err) {
+		if (err) {
+			throw err;
+		}
+	})
+}
+function arrayToData(array,field){
+	var message="";
+	for(var i=0;i<array.length;i++){
+		message+=array[i][field]+'\r\n'
+	}
+	return message;
+}
+(async () =>{
+	try {
+		var data=await getFullTableScope(tokenContract,tokenTable)//这里填写账户名、表名. You should fill in this fields with accounts name and table name;
+		await writeFile('./airdrop_snapshots.txt',arrayToData(data,'scope'));
+		console.log("Total accounts:"+data.length);
+	} catch (e) {
+		console.log(e);
+	}
+})();
